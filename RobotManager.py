429f891b1caa9e2fb2fdec3dee1f99256370c874
Variante <382086919@qq.com>
2025-03-21 00:45:27 -0400

    optimize params

--- RobotManager.py
+++ RobotManager.py
@@ -55,7 +55,7 @@ class RobotManager:
         self.calib_p = None
         self.frame_top = None
         self.corners = None
-        self.pickup_p = (115, -171) # location to pickup the checker
+        self.pickup_p = (110, -168) # location to pickup the checker
         self.pickup_c = 0 # counter for num of checkers we picked it up
         """
         options = apriltag.Detectoroptions(families='tag36h11',
@@ -227,65 +227,22 @@ class RobotManager:
         else:
             self.corners = cl
 
-        def order_points(pts):
-            # initialzie a list of coordinates that will be ordered
-            # such that the first entry in the list is the top-left,
-            # the second entry is the top-right, the third is the
-            # bottom-right, and the fourth is the bottom-left
-            rect = np.zeros((4, 2), dtype="float32")
-            # the top-left point will have the smallest sum, whereas
-            # the bottom-right point will have the largest sum
-            s = pts.sum(axis=1)
-            rect[0] = pts[np.argmin(s)]
-            rect[2] = pts[np.argmax(s)]
-            # now, compute the difference between the points, the
-            # top-right point will have the smallest difference,
-            # whereas the bottom-left will have the largest difference
-            diff = np.diff(pts, axis=1)
-            rect[1] = pts[np.argmin(diff)]
-            rect[3] = pts[np.argmax(diff)]
-            # return the ordered coordinates
-            return rect
-
-        def four_point_transform(image, pts):
-            # obtain a consistent order of the points and unpack them
-            # individually
-            rect = order_points(pts)
-            (tl, tr, br, bl) = rect
-            """
-            # compute the width of the new image, which will be the
-            # maximum distance between bottom-right and bottom-left
-            # x-coordiates or the top-right and top-left x-coordinates
-            widthA = np.sqrt(((br[0] - bl[0]) ** 2) + ((br[1] - bl[1]) ** 2))
-            widthB = np.sqrt(((tr[0] - tl[0]) ** 2) + ((tr[1] - tl[1]) ** 2))
-            maxWidth = max(int(widthA), int(widthB))
-            # compute the height of the new image, which will be the
-            # maximum distance between the top-right and bottom-right
-            # y-coordinates or the top-left and bottom-left y-coordinates
-            heightA = np.sqrt(((tr[0] - br[0]) ** 2) + ((tr[1] - br[1]) ** 2))
-            heightB = np.sqrt(((tl[0] - bl[0]) ** 2) + ((tl[1] - bl[1]) ** 2))
-            maxHeight = max(int(heightA), int(heightB))
-            # now that we have the dimensions of the new image, construct
-            # the set of destination points to obtain a "birds eye view",
-            # (i.e. top-down view) of the image, again specifying points
-            # in the top-left, top-right, bottom-right, and bottom-left
-            # order
-            """
-            maxWidth = 400
-            maxHeight = 300
-            dst = np.array([
-                [0, 0],
-                [maxWidth - 1, 0],
-                [maxWidth - 1, maxHeight - 1],
-                [0, maxHeight - 1]], dtype="float32")
-            # compute the perspective transform matrix and then apply it
-            M = cv2.getPerspectiveTransform(rect, dst)
-            invM = cv2.getPerspectiveTransform(dst, rect)
-            warped = cv2.warpPerspective(image, M, (maxWidth, maxHeight))
+        rect = np.array(self.corners).reshape(4, 2).astype('float32')
+        maxWidth = 300
+        maxHeight = 400
+        dst = np.array([
+            [0, 0],
+            [maxWidth - 1, 0],
+            [0, maxHeight - 1],
+            [maxWidth - 1, maxHeight - 1]
+            ], dtype="float32")
+        # compute the perspective transform matrix and then apply it
+        M = cv2.getPerspectiveTransform(rect, dst)
+        invM = cv2.getPerspectiveTransform(dst, rect)
+        warped = cv2.warpPerspective(self.frame_top, M, (maxWidth, maxHeight))
             # return the warped image
-            return warped, invM
+        return warped, invM
 
-        return four_point_transform(self.frame_top, np.array(cl))
 
     def img_to_board(self, warp):
         img, invM = warp
@@ -305,10 +262,10 @@ class RobotManager:
         h = self.b_height
         # availables = []
         states = {0: [], 1: [], 2:[]}
-        wleft = 60
-        ww = 53
-        htop = 12
-        hh = 55
+        wleft = 11
+        ww = 55
+        htop = 70
+        hh = 53
 
         hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
 
@@ -495,7 +452,12 @@ class RobotManager:
     def flip(self):
         # self.win()
         self.parse_board()
-        pos = self.query_position((self.corners[1] + self.corners[0]) / 2)
+        pos1 = self.query_position((self.corners[0] + self.corners[1]) / 2)
+        pos2 = self.query_position((self.corners[2] + self.corners[3]) / 2)
+        if pos1[0] < pos2[0]:
+            pos = pos1
+        else:
+            pos = pos2
         self.swift.set_position(x=int(pos[0] + 5), y=int(pos[1]), z=10, wait=True, speed=1000000)
         self.swift.set_position(x=int(pos[0] + 10), y=int(pos[1]), z=-5, wait=True, speed=1000000)
         time.sleep(1)
